165769dd2170d561faf515546ef9b350dd35dd64
muhaj-dev <muhammodholawale@gmail.com>
2025-10-19 00:58:46 +0100

    bug fix

--- next.config.js
+++ next.config.js
@@ -1,32 +1,6 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {}
-
-// module.exports = nextConfig
-
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Add these experimental options
-  experimental: {
-    webpackBuildWorker: true,
-    parallelServerBuildTraces: false,
-    parallelServerCompiles: false,
-  },
-  
-  // Disable source maps in production
-  productionBrowserSourceMaps: false,
-  
-  // Optimize for Vercel
-  compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
-  },
-  
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
+
+
