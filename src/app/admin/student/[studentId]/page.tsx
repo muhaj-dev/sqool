761027a4a896f059728dc8a9cf052a8e828bf801
muhaj-dev <muhammodholawale@gmail.com>
2025-10-27 04:14:59 +0100

    fees admin

--- src/app/admin/student/[studentId]/page.tsx
+++ src/app/admin/student/[studentId]/page.tsx
@@ -1,18 +1,36 @@
 
+
+// interface PageParams {
+//   studentId: string;
+// }
+
+// const Page = async ({
+//   params,
+// }: {
+//   params: Promise<PageParams>;
+// }) => {
+//   const { studentId } = await params;
+
+//   return <StudentPageClient studentId={studentId} />;
+// };
+
+// export default Page;
+
+'use client'
+import { use } from 'react'
 import StudentPageClient from "./StudentPageClient";
-
-interface PageParams {
-  studentId: string;
-}
-
-const Page = async ({
+ 
+export default function StudentPage({
   params,
 }: {
-  params: Promise<PageParams>;
-}) => {
-  const { studentId } = await params;
-
-  return <StudentPageClient studentId={studentId} />;
-};
-
-export default Page;
+  params: Promise<{ studentId: string }>
+}) {
+  const { studentId } = use(params)
+ 
+  return (
+    <div>
+      {/* <p>{studentId}</p> */}
+       <StudentPageClient studentId={studentId} />
+    </div>
+  )
+}
