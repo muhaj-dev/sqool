cb90735881c3fe68c8fcd236af50b123a01139eb
muhaj-dev <muhammodholawale@gmail.com>
2025-10-28 04:40:10 +0100

    ii

--- next.config.js
+++ next.config.js
@@ -21,24 +21,12 @@ module.exports = nextConfig
 // module.exports = nextConfig;
 
 
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   swcMinify: true,
-//   experimental: {
-//     outputFileTracingExcludes: {
-//       '*': [
-//         '**/.next/**',
-//         '**/node_modules/**',
-//         '**/.git/**',
-//         '**/.vercel/**',
-//         '**/pnpm-lock.yaml',
-//       ],
-//     },
-//   },
-//   eslint: {
-//     ignoreDuringBuilds: true, // ✅ disables ESLint on build
-//   },
-// };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
-// module.exports = nextConfig;
+module.exports = nextConfig;
