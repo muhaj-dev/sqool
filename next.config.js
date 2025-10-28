96ef10cd13c1fb2884cd7e01c77e8b5af5741b95
muhaj-dev <muhammodholawale@gmail.com>
2025-10-28 04:40:58 +0100

    ii

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
 
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
